b956a51d17137c8e1c8cd543ee59dc5196311f3d
drenmorina12 <drenmorina321@gmail.com>
2023-12-22 10:45:40 +0100

    Add necessary table info

---
+++ argetim/argetimi.js
@@ -0,0 +1,21 @@
+const asosacioni = document.querySelector("#asosacioni");
+
+const columns = ["A", "B", "C", "D"];
+
+const words = {
+  A: ["A1", "A2", "A3", "A4"],
+  B: ["B1", "B2", "B3", "B4"],
+  C: ["C1", "C2", "C3", "C4"],
+  D: ["D1", "D2", "D3", "D4"],
+};
+
+const solutions = {
+  A: "solution1",
+  B: "solution2",
+  C: "solution3",
+  D: "solution4",
+};
+
+const finalResult = "Zgjidhja Perfundimtare";
+
+function createTable() {}
