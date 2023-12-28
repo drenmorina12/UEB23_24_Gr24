331b4d2b4d3bcfface360bcb8df4e92326abeec6
drenmorina12 <drenmorina321@gmail.com>
2023-12-28 23:35:39 +0100

    Add function that changes cell content on click
    add "data-column" attribute to cells.
    add template list and word list.

--- argetim/argetim.js
+++ argetim/argetim.js
@@ -3,17 +3,24 @@ const asosacioni = document.querySelector("#asosacioni");
 const columns = ["A", "B", "C", "D"];
 
 const solutions = {
-  A: "Zgjidhja A",
-  B: "Zgjidhja B",
-  C: "Zgjidhja C",
-  D: "Zgjidhja D",
+  A: "Zgjidhja A TEST",
+  B: "Zgjidhja B TEST",
+  C: "Zgjidhja C TEST",
+  D: "Zgjidhja D TEST",
+};
+
+const template = {
+  A: ["A1", "A2", "A3", "A4", "Zgjidhja A"],
+  B: ["B1", "B2", "B3", "B4", "Zgjidhja B"],
+  C: ["C1", "C2", "C3", "C4", "Zgjidhja C"],
+  D: ["D1", "D2", "D3", "D4", "Zgjidhja D"],
 };
 
 const words = {
-  A: ["A1", "A2", "A3", "A4", solutions.A],
-  B: ["B1", "B2", "B3", "B4", solutions.B],
-  C: ["C1", "C2", "C3", "C4", solutions.C],
-  D: ["D1", "D2", "D3", "D4", solutions.D],
+  A: ["A1-test", "A2-test", "A3-test", "A4-test", solutions.A],
+  B: ["B1-test", "B2-test", "B3-test", "B4-test", solutions.B],
+  C: ["C1-test", "C2-test", "C3-test", "C4-test", solutions.C],
+  D: ["D1-test", "D2-test", "D3-test", "D4-test", solutions.D],
 };
 
 const finalResult = "Zgjidhja Perfundimtare";
@@ -26,7 +33,9 @@ function createTable() {
     columns.forEach((column) => {
       const cell = document.createElement("div");
       cell.className = "cell";
-      cell.textContent = words[column][i].toLocaleUpperCase();
+      cell.textContent = template[column][i].toLocaleUpperCase();
+      cell.setAttribute("data-column", column);
+
       row.appendChild(cell);
     });
 
@@ -50,6 +59,14 @@ const cells = createTable();
 
 cells.forEach((cell) => {
   cell.addEventListener("click", (e) => {
-    console.log(e);
+    const column = e.currentTarget.getAttribute("data-column");
+
+    const row = Array.from(
+      e.currentTarget.parentElement.parentElement.children
+    ).indexOf(e.currentTarget.parentElement);
+
+    cell.textContent = words[column][row];
+    cell.style.cssText =
+      "background-color: white; color: #002244; border: 2px solid #002244";
   });
 });
