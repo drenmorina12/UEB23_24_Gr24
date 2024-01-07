80d89a9f407a358b5a4d5ebe12eaeb6889d1e7dc
drenmorina12 <drenmorina321@gmail.com>
2024-01-07 21:05:52 +0100

    Start adding js logic for memorey game, add basic css

---
+++ argetim/memory-game.js
@@ -0,0 +1,89 @@
+const gridDisplay = document.querySelector("#grid");
+const resultDisplay = document.querySelector("#result");
+
+const cardArray = [
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+];
+
+let cardsChosen = [];
+let cardsChosenIds = [];
+let isChecking = false;
+
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank-1.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.append(card);
+  }
+}
+
+function flipCard() {
+  if (isChecking) {
+    return;
+  }
+
+  const cardId = this.getAttribute("data-id");
+
+  if (cardsChosenIds[0] !== cardId) {
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+  }
+
+  console.log("Clicked", cardId);
+  this.setAttribute("src", cardArray[cardId].img);
+  if (cardsChosen.length === 2) {
+    isChecking = true;
+    setTimeout(checkMatch, 500);
+  }
+}
+
+function checkMatch() {}
