18631499e45de3b21e009bb79cd7aff12fd94d62
drenmorina12 <drenmorina321@gmail.com>
2024-01-08 01:08:13 +0100

    Add checkWin() function and startGame() function

--- argetim/tic-tac-toe.js
+++ argetim/tic-tac-toe.js
@@ -3,17 +3,35 @@ const board = document.querySelector("#board");
 
 const X_CLASS = "x";
 const CIRLCE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [1, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 let circleTurn;
 
-cellElements.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
+startGame();
+
+function startGame() {
+  cellElements.forEach((cell) => {
+    circleTurn = false;
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
 
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRLCE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
-  // placeMark
+  if (checkWin(currentClass)) {
+    console.log("WINNER");
+  }
   // checkForWin
   // checkForDraw
   // switchTurns
@@ -37,4 +55,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS);
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
